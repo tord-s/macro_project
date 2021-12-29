64b4f4b441c8540a08c0e20f4a277c76d84c51bf
tord-s <tord@softeland.name>
2021-12-29 20:55:17 +0100

    changed format on sam

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import send_file, render_template, request
 import pandas as pd
+import numpy as np
 import os
 import time
 
@@ -15,44 +16,28 @@ try:
         excel_base_path = 'excel_files/'
 except:
     pass
-default_sam_file = excel_base_path + 'sam.xlsx'
-default_euklems_file = excel_base_path + 'euklems.xlsx'
-default_mapping_file = excel_base_path + 'mapping.xlsx'
+default_sam_file = excel_base_path + 'sam_long.xlsx'
+default_euklems_file = excel_base_path + 'euklems ny.xlsx'
+# default_mapping_file = excel_base_path + 'mapping.xlsx'
 euklems_codes_column_name_in_excel = "EUKLEMS code"
 sam_codes_column_name_in_excel = "SAM code"
 
 
-def do_calculations(sam_file=default_sam_file, euklems_file=default_euklems_file, mapping_file=default_mapping_file,
-                    sam_sheet_name='AT', euklems_sheet_name='W_shares', mapping_sheet_name='Sheet1', year_of_analysis='2017', country_code='AT'):
+def do_calculations(sam_file=default_sam_file, euklems_file=default_euklems_file,
+                    sam_sheet_name='Sheet1', euklems_sheet_name='W_shares', mapping_sheet_name='Mapping', year_of_analysis='2017', country_code='AT'):
     start_time = time.time()
 
     """
     Generating the Pandas Dataframes from the files
     """
-    # sam = pd.read_excel(sam_file, sheet_name=sam_sheet_name,
-    #                     index_col=0)
     euklems = pd.read_excel(euklems_file, sheet_name=euklems_sheet_name)
-    mapping = pd.read_excel(mapping_file, sheet_name=mapping_sheet_name)
-
-    # Reads the Labour row of the SAM - Generates a dictionary
-    # sam_labour_row = sam.loc['Labour'].dropna().to_dict()
-
-    # print(sam_labour_row)
-    # print('----------------------------------------')
-
-    # sam_long = pd.read_excel('excel_files/sam_long.xlsx', sheet_name='Sheet1',
-    #                          names=['country', 'gets', 'gives', 'amount'])
-    sam_long = pd.read_excel(sam_file, sheet_name='Sheet1',
+    mapping = pd.read_excel(euklems_file, sheet_name=mapping_sheet_name)
+    sam_long = pd.read_excel(sam_file, sheet_name=sam_sheet_name,
                              names=['country', 'gets', 'gives', 'amount'])
 
     sam_for_given_country = sam_long.loc[sam_long['country'] == country_code]
     sam_long_labour_row = sam_for_given_country.loc[sam_for_given_country['gets'] == 'Labour', [
         'amount', 'gives']].set_index('gives')['amount'].to_dict()
-    # print(sam_for_given_country)
-    # print('----------------------------------------')
-    # print(sam_long_labour_row)
-
-    sam_labour_row = sam_long_labour_row
 
     """
     Mapping codes SAM/EUKLEMS
@@ -74,7 +59,7 @@ def do_calculations(sam_file=default_sam_file, euklems_file=default_euklems_file
         sam_total_per_sector = 0
         codes_from_sam = code_map[0]
         for code_from_sam in codes_from_sam:
-            sam_total += sam_labour_row[code_from_sam.strip()]
+            sam_total += sam_long_labour_row[code_from_sam.strip()]
         sam_total_per_sector = sam_total / len(codes_from_sam)
 
         euklems_relevant_data = euklems[[
@@ -92,47 +77,62 @@ def do_calculations(sam_file=default_sam_file, euklems_file=default_euklems_file
                     data_to_be_added.append(('Labour ' + combo,
                                             code_from_sam, new_value_in_sam))
 
-    """
-    Big Question here - does he want output in 'Long' format as well?
-    """                                            
-    output_sceleton = sam
-    # Gets index of where the Labour column and row is
-    labour_column_index = output_sceleton.columns.get_loc("Labour")
-    labour_row_index = list(output_sceleton.index).index("Labour")
+    # Makes the df as small as possible to make it quicker to search
+    smaller_index_search_df = sam_long.loc[(sam_long['country'] == country_code) & (
+        sam_long['gets'] == 'Labour')]
 
-    # Adds the colums where labour is now
+    # The SAM codes in order
+    list_of_sam_codes = list(smaller_index_search_df['gives'])
+
+    testing_dict = {}
+
+    for sam_code in list_of_sam_codes:
+        total_sum = float(sam_long.loc[(sam_long['country'] == country_code) & (
+            sam_long['gets'] == 'Labour') & (
+            sam_long['gives'] == sam_code)].amount)
+        testing_dict[sam_code] = {'total_sum': total_sum, 'sum_of_parts': 0}
+
+    """
+    Formating the data to be added to a nested dictonary to make it easy to look up when inserting data
+    """
+    formated_data = {}
+    for value in data_to_be_added:
+        sam_code, labour_combo, amount = value[1], value[0], value[2]
+        try:
+            defined = formated_data[sam_code]
+        except:
+            formated_data[sam_code] = {}
+        testing_dict[sam_code]['sum_of_parts'] = testing_dict[sam_code]['sum_of_parts'] + amount
+        formated_data[sam_code][labour_combo] = amount
+
+    test_success = True
+    for key, value in testing_dict.items():
+        if value['total_sum'] != round(value['sum_of_parts'], 2):
+            print('ERROR with ', key, ' giving unequal sum ', value)
+            test_success = False
+    print('Test success: ', test_success)
+
+    # The sceleton to be populated with data
+    output_sceleton = sam_long.loc[(sam_long['country'] == country_code)]
+
+    # Gets the index to insert the new rows
+    labour_row_index_list = list(
+        smaller_index_search_df.loc[sam_long['gets'] == 'Labour'].index)
+    labour_row_index = int(labour_row_index_list[0]) - 1
+
+    # Inserts the new rows
     i = len(combinations) - 1
-    while (i > 0):
-        output_sceleton.insert(labour_column_index - 1,
-                               'Labour ' + combinations[i], None)
+    while (i >= 0):
+        for code in list_of_sam_codes:
+            combination = 'Labour ' + combinations[i]
+            output_sceleton = pd.DataFrame(np.insert(output_sceleton.values, labour_row_index + 1, values=[
+                country_code, combination, code, formated_data[code][combination]], axis=0))
         i -= 1
 
-    # Splits data to be able to insert between rows
-    first_part = output_sceleton.copy(deep=True).iloc[0:labour_row_index]
-    second_part = output_sceleton.copy(deep=True).iloc[labour_row_index:]
-
-    # inserts the data in the right rows and columns
-    for row, column, value in data_to_be_added:
-        first_part.loc[row, column] = value
-
-    # Merges the data again after the rows have been inserted
-    output_sceleton = first_part.append(second_part)
-
-    for combo in combinations:
-        combo_values = list(output_sceleton.loc['Labour ' +
-                                                combo].dropna().to_dict().values())
-        combo_total = 0
-        for value in combo_values:
-            combo_total += float(value)
-        output_sceleton.loc['HOUS', 'Labour ' + combo] = combo_total
-
-    # Removes more general Labour column and row
-    output_sceleton.drop(['Labour'], axis=1, inplace=True)
-    output_sceleton.drop(['Labour'], axis=0, inplace=True)
-
+    # Creates the excel file with the populated sceleton
     with pd.ExcelWriter(excel_base_path + "output.xlsx") as writer:
-        output_sceleton.to_excel(writer, verbose=True,
-                                 index=True, sheet_name='Results')
+        output_sceleton.to_excel(writer, verbose=False, header=False,
+                                 index=False, sheet_name='Results')
     end_time = time.time()
     return round(end_time-start_time, 5)
 
@@ -153,18 +153,17 @@ def return_files_tut():
     if request.method == 'POST':
         sam = request.files['sam']
         euklems = request.files['euklems']
-        mapping = request.files['mapping']
         sam_sheet_name = request.form['sam_sheet_name']
         euklems_sheet_name = request.form['euklems_sheet_name']
         mapping_sheet_name = request.form['mapping_sheet_name']
         year_of_analysis = request.form['year_of_analysis']
-        print(sam_sheet_name)
         used_time = do_calculations(
-            sam, euklems, mapping, sam_sheet_name, euklems_sheet_name, mapping_sheet_name, year_of_analysis)
+            sam_file=sam, euklems_file=euklems, sam_sheet_name=sam_sheet_name, euklems_sheet_name=euklems_sheet_name,
+            mapping_sheet_name=mapping_sheet_name, year_of_analysis=year_of_analysis)
         print('Used time: ', used_time)
-        return send_file(excel_base_path + "output.xlsx", attachment_filename='resulting_output_id' + str(round(time.time(), 0))[: -2] + '.xlsx')
+        return send_file(excel_base_path + "output.xlsx", attachment_filename='Extented SAM (timestamp ' + str(round(time.time(), 0))[: -2] + ').xlsx')
     try:
-        return send_file(excel_base_path + "output.xlsx", attachment_filename='resulting_output_id' + str(round(time.time(), 0))[: -2] + '.xlsx')
+        return send_file(excel_base_path + "output.xlsx", attachment_filename='Extented SAM (timestamp ' + str(round(time.time(), 0))[: -2] + '.xlsx')
     except Exception as e:
         return str(e)
 
